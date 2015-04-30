0cd23de85daa0809f5d7de0bcb3f7ceb36b2dc22
Geronimo Romney <g.romney@gmail.com>
2015-04-30 12:30:14 -0400

    adding ImageAdapter

--- app/src/main/java/com/pseudolab/gridview_sdcard/MainActivity.java
+++ app/src/main/java/com/pseudolab/gridview_sdcard/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         gridView = (GridView)findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
     }
 
 
